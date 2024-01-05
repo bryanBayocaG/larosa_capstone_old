0fa5c9ef74677bc56aee824e1056284107ffa51d
Bryan Bayoca <123434159+binayoca686@users.noreply.github.com>
2024-01-05 13:23:31 +0800

    title

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -50,7 +50,7 @@
 <html lang="en">
 
 <head>
-    <title>Larosa</title>
+    <title>Larosa Gowns and Rental</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
