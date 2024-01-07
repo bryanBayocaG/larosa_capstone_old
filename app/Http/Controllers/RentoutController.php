67ff71c804621528b9465f48c8431c174115bf32
Bryan Bayoca <123434159+binayoca686@users.noreply.github.com>
2024-01-07 22:47:50 +0800

    text

--- app/Http/Controllers/RentoutController.php
+++ app/Http/Controllers/RentoutController.php
@@ -14,6 +14,7 @@ use App\Models\RentInfo;
 use Gloudemans\Shoppingcart\Facades\Cart;
 use Illuminate\Http\Request;
 use Carbon\Carbon;
+use DateTime;
 use Illuminate\Support\Str;
 use GuzzleHttp\Client;
 use Illuminate\Support\Facades\DB;
@@ -36,14 +37,14 @@ class RentoutController extends Controller
 
         $tot = Cart::instance('shopping')->priceTotal();
         $totalPrice = str_replace(',', '', $tot);
-        $request->validate([
-            'fname' => 'required|string|max:255',
-            'lname' => 'required|string|max:255',
-            'contactnum' => 'required|string|max:255',
-            'eventdate' => 'required|date',
-            'address' => 'required|string|max:255',
-            'payment' => 'required|string|max:255'
-        ]);
+        // $request->validate([
+        //     'fname' => 'required|string|max:255',
+        //     'lname' => 'required|string|max:255',
+        //     'contactnum' => 'required|string|max:255',
+        //     'eventdate' => 'required|date',
+        //     'address' => 'required|string|max:255',
+        //     'payment' => 'required|string|max:255'
+        // ]);
         $randomCode = Str::random(10);
 
         $bayad = $request->input('payment');
@@ -52,76 +53,76 @@ class RentoutController extends Controller
         $eventDate = Carbon::parse($request->input('eventdate'));
         $returnDate = $eventDate->addDays(3);
 
-        $RentinfoId = DB::table('rent_infos')->insertGetId([
-            'transac_code' => $randomCode,
-            'first_name' => $request->input('fname'),
-            'last_name' => $request->input('lname'),
-            'contact_num' => $request->input('contactnum'),
-            'address' => $request->input('address'),
-            'rent_type' => $request->input('rent_type'),
-            'event_date' => $request->input('eventdate'),
-            'totalPrice' => $totalPrice,
-            'balance' => $balance,
-            'return_date' => $returnDate,
-            'created_at' => now(),
-            'updated_at' => now(),
-        ]);
+        // $RentinfoId = DB::table('rent_infos')->insertGetId([
+        //     'transac_code' => $randomCode,
+        //     'first_name' => $request->input('fname'),
+        //     'last_name' => $request->input('lname'),
+        //     'contact_num' => $request->input('contactnum'),
+        //     'address' => $request->input('address'),
+        //     'rent_type' => $request->input('rent_type'),
+        //     'event_date' => $request->input('eventdate'),
+        //     'totalPrice' => $totalPrice,
+        //     'balance' => $balance,
+        //     'return_date' => $returnDate,
+        //     'created_at' => now(),
+        //     'updated_at' => now(),
+        // ]);
 
-        foreach (Cart::instance('shopping')->content() as $item) {
-            $orderItem = new rentedItem();
-            $orderItem->rent_info_id = $RentinfoId;
-            // $orderItem->product_set_id = $item->id;
-            if ($item->options->category === '') {
-                $orderItem->product_set_id = $item->id;
-                $orderItem->single_item_id = null;
-            } else {
-                $orderItem->product_set_id = null;
-                $orderItem->single_item_id = $item->id;
-            }
-            $orderItem->status = 'Rented';
-            $orderItem->pricing = $item->price;
-            $orderItem->quantity = intval($item->qty);
-            $orderItem->save();
+        // foreach (Cart::instance('shopping')->content() as $item) {
+        //     $orderItem = new rentedItem();
+        //     $orderItem->rent_info_id = $RentinfoId;
+        //     // $orderItem->product_set_id = $item->id;
+        //     if ($item->options->category === '') {
+        //         $orderItem->product_set_id = $item->id;
+        //         $orderItem->single_item_id = null;
+        //     } else {
+        //         $orderItem->product_set_id = null;
+        //         $orderItem->single_item_id = $item->id;
+        //     }
+        //     $orderItem->status = 'Rented';
+        //     $orderItem->pricing = $item->price;
+        //     $orderItem->quantity = intval($item->qty);
+        //     $orderItem->save();
 
-            $product = product_set::find($item->id);
-            $Sitem = item::find($item->id);
-            if ($product) {
-                $product->remaining = $product->remaining - $item->qty;
-                $product->save();
-                for ($i = 0; $i < intval($item->qty); $i++) {
-                    $includedItems = included_item::where('product_set_id', $product->id)->get();
-                    foreach ($includedItems as $includedItem) {
-                        $itemDetail = item_details::where('item_id', $includedItem->item_id)->where('status', 'in-possesion')->first();
-                        if ($itemDetail) {
-                            $itemDetail->set_id = $RentinfoId;
-                            $itemDetail->status = 'Rented';
-                            $itemDetail->save();
-                        }
-                    }
-                }
-            }
-            if ($Sitem) {
-                $itemQuan = Item_quantity::where('item_id', $Sitem->id)->first();
-                if ($itemQuan) {
-                    $itemQuan->remaining = $itemQuan->remaining - $item->qty;
-                    $itemQuan->save();
-                }
-                for ($i = 0; $i < intval($item->qty); $i++) {
-                    $itemBelongs = Item_details::where('item_id', $Sitem->id)->where('status', 'in-possesion')->first();
-                    if ($itemBelongs) {
-                        $itemBelongs->set_id = $RentinfoId;
-                        $itemBelongs->status = 'Rented';
-                        $itemBelongs->save();
-                    }
-                }
-            }
-        };
+        //     $product = product_set::find($item->id);
+        //     $Sitem = item::find($item->id);
+        //     if ($product) {
+        //         $product->remaining = $product->remaining - $item->qty;
+        //         $product->save();
+        //         for ($i = 0; $i < intval($item->qty); $i++) {
+        //             $includedItems = included_item::where('product_set_id', $product->id)->get();
+        //             foreach ($includedItems as $includedItem) {
+        //                 $itemDetail = item_details::where('item_id', $includedItem->item_id)->where('status', 'in-possesion')->first();
+        //                 if ($itemDetail) {
+        //                     $itemDetail->set_id = $RentinfoId;
+        //                     $itemDetail->status = 'Rented';
+        //                     $itemDetail->save();
+        //                 }
+        //             }
+        //         }
+        //     }
+        //     if ($Sitem) {
+        //         $itemQuan = Item_quantity::where('item_id', $Sitem->id)->first();
+        //         if ($itemQuan) {
+        //             $itemQuan->remaining = $itemQuan->remaining - $item->qty;
+        //             $itemQuan->save();
+        //         }
+        //         for ($i = 0; $i < intval($item->qty); $i++) {
+        //             $itemBelongs = Item_details::where('item_id', $Sitem->id)->where('status', 'in-possesion')->first();
+        //             if ($itemBelongs) {
+        //                 $itemBelongs->set_id = $RentinfoId;
+        //                 $itemBelongs->status = 'Rented';
+        //                 $itemBelongs->save();
+        //             }
+        //         }
+        //     }
+        // };
 
-        $payment = new payment();
-        $payment->rent_info_id = $RentinfoId;
-        $payment->payments = $request->input('payment');
-        $payment->remarks = $request->input('remarks');
-        $payment->save();
+        // $payment = new payment();
+        // $payment->rent_info_id = $RentinfoId;
+        // $payment->payments = $request->input('payment');
+        // $payment->remarks = $request->input('remarks');
+        // $payment->save();
 
 
 
@@ -131,12 +132,23 @@ class RentoutController extends Controller
         $itemNames = '';
         foreach (Cart::instance('shopping')->content() as $item) {
             $itemName = $item->name;
-            $itemNames .= ($itemNames ? ', ' : '') . $itemName;
+            $itemQuantity = $item->qty;
+            $itemNames .= ($itemNames ? ', ' : '') . $itemQuantity . 'x ' . $itemName;
         }
         $customerFName = $request->input('fname');
         $customerLName = $request->input('lname');
-        $message = "Hello " . $customerFName . " " . $customerLName . " as of " . now() . ", you rented the following item(s) [" . $itemNames . "] with a total of " . $tot . ", and paid by the amount of " . $bayad . ", Thank you for renting in Larosa";
+        $returnDateObject = Carbon::parse($returnDate);
+        $formattedReturnDate = $returnDateObject->format('Y-m-d');
 
+        $message = "Hello, " . $customerFName . " " . $customerLName . ",\n\n"
+            . "As of " . now() . ", You have successfully rented the following item(s) [" . $itemNames . "] with a total Pricing of ₱" . $tot . ","
+            . "and paid by the total amount of ₱" . number_format($bayad, 2, '.', ',') . ".\n"
+            . "With rental period starting today until the date of " . $formattedReturnDate . ".\n"
+            . "Please make sure to take good care of the item(s) during the rental period. \n\n"
+            . "Thank you for choosing La Rosa rental service!";
+
+        $returnDateObject = Carbon::parse($returnDate);
+        $formattedReturnDate = $returnDateObject->format('m d y');
 
         $client = new Client();
         $client->post('https://semaphore.co/api/v4/messages', [
