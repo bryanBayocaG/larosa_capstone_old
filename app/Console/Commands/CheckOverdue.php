cb066ed9b311bfe24e80544c9ecf718ce00dc5ac
Bryan Bayoca <123434159+binayoca686@users.noreply.github.com>
2024-04-11 22:11:06 +0800

    hayp2

--- app/Console/Commands/CheckOverdue.php
+++ app/Console/Commands/CheckOverdue.php
@@ -55,7 +55,7 @@ class CheckOverdue extends Command
         //     ]);
         // }
 
-            $message = 'Dear hoyhoyhoy testing sang cron sa hostinger';
+            $message = 'Dear hoyhoyhoy testing sang cron sa hostinger sa 10:15';
             $client = new Client();
             $client->post('https://semaphore.co/api/v4/messages', [
                 'form_params' => [
