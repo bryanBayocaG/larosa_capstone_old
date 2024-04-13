40435938dd9ad5194468a83b7fd1491786ca2fee
Bryan Bayoca <123434159+binayoca686@users.noreply.github.com>
2024-04-13 20:44:11 +0800

    haha

--- app/Console/Commands/CheckOverdue.php
+++ app/Console/Commands/CheckOverdue.php
@@ -31,40 +31,40 @@ class CheckOverdue extends Command
     {
         $apiKey = 'da96e8b9124390d6e9a85bfc253e6cea';
 
-        // $currentDate = Carbon::now()->format('Y-m-d');
+        $currentDate = Carbon::now()->format('Y-m-d');
 
-        // DB::table('rent_infos')
-        //     ->where('return_date', '<', $currentDate)
-        //     ->where('status', 'Renting')
-        //     ->update(['status' => 'Overdue']);
+        DB::table('rent_infos')
+            ->where('return_date', '<', $currentDate)
+            ->where('status', 'Renting')
+            ->update(['status' => 'Overdue']);
 
-        // $overDueRent = RentInfo::where('status', 'Overdue')->get();
+        $overDueRent = RentInfo::where('status', 'Overdue')->get();
 
-        // foreach ($overDueRent as $Orent) {
-        //     $message = 'Dear ' . $Orent->first_name . ' ' . $Orent->first_name . ', we would like to inform you about the Rent you had made is Overdue, this will result in having a fine of 300 pesos penalty each day this rented Item/Set is not returned.';
-        //     $cleanedNumber = str_replace('-', '', $Orent->contact_num);
-        //     $client = new Client();
-        //     $client->post('https://semaphore.co/api/v4/messages', [
-        //         'form_params' => [
-        //             'apikey' => $apiKey,
-        //             'number' => $cleanedNumber,
-        //             'message' => $message,
-        //             'sendername' => 'LAROSA',
-        //         ],
-        //         'verify' => false,
-        //     ]);
-        // }
-
-            $message = 'Dear hoyhoyhoy testing sang dynanimic';
+        foreach ($overDueRent as $Orent) {
+            $message = 'Dear ' . $Orent->first_name . ' ' . $Orent->first_name . ', we would like to inform you about the Rent you had made is Overdue, this will result in having a fine of 300 pesos penalty each day this rented Item/Set is not returned.';
+            $cleanedNumber = str_replace('-', '', $Orent->contact_num);
             $client = new Client();
             $client->post('https://semaphore.co/api/v4/messages', [
                 'form_params' => [
                     'apikey' => $apiKey,
-                    'number' => '09667984996',
+                    'number' => $cleanedNumber,
                     'message' => $message,
                     'sendername' => 'LAROSA',
                 ],
                 'verify' => false,
             ]);
+        }
+
+            // $message = 'Dear hoyhoyhoy testing sang dynanimic';
+            // $client = new Client();
+            // $client->post('https://semaphore.co/api/v4/messages', [
+            //     'form_params' => [
+            //         'apikey' => $apiKey,
+            //         'number' => '09667984996',
+            //         'message' => $message,
+            //         'sendername' => 'LAROSA',
+            //     ],
+            //     'verify' => false,
+            // ]);
     }
 }
